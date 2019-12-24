f6c94bd89e3fae945482d46130229d8379f8d3be
sregea <aitorpegea@gmail.com>
2019-12-24 15:23:21 +0100

    Day 10 second part

--- 10/problem10.py
+++ 10/problem10.py
@@ -1,6 +1,12 @@
 import math
+import sys
+from collections import defaultdict
 
+# Decide the name of the file.
 FILENAME = 'input.dat'
+if len(sys.argv) == 2:
+    FILENAME = sys.argv[1]
+
 
 class Place():
     def __init__(self, i, j, asteroid):
@@ -9,19 +15,39 @@ class Place():
         self.asteroid = asteroid
         self.visible = True
 
-def main(fileName):
+# Class to solve the second part. Stores info about the angle
+# and the distance of an object to a monitoring station.
+class AngleInfo():
+    def __init__(self, x, y, xstation, ystation):
+        self.x = x
+        self.y = y
+        self.angle = angle(xstation, ystation, x, y)
+        self.dist = distance(xstation, ystation, x, y)
+
+
+def main(fileName, full):
     # Get the input from the file.
     with open(fileName, 'r') as file:
         rawLines = file.readlines()
     
     # Parse the data into a list of asteroids.
-    asteroids = parseRawInput(rawLines)
+    grid = parseRawInput(rawLines)
 
-    # Find the result and print it.
-    result = findBestLocation(asteroids['asteroids'], asteroids['number'])
+    # Find the best location for the station and print it.
+    result = findBestLocation(grid)
     print('Best location for the station: ({},{})'.format(result['bestx'], result['besty']))
     print('Number of asteroids in sight: {}'.format(result['visibles']))
 
+    if not full:
+        return result
+
+    # Find the location of the asteroid that gets vaporized the 200th.
+    vaporized = findVaporized200(grid, result['bestx'], result['besty'])
+    print('Asteroid vaporized in 200th place is at ({},{})'.format(vaporized['x'], vaporized['y']))
+
+    # Add to the results.
+    result['vaporizedx'] = vaporized['x']
+    result['vaporizedy'] = vaporized['y']
     return result
 
 # Parse raw lines to a list of asteroid objects.
@@ -38,16 +64,16 @@ def parseRawInput(lines):
             elif lines[i][j] == '.':
                 asteroids[i].append(Place(i, j, False))
 
-    return {'asteroids':asteroids, 'number':number}
+    return {'asteroids':asteroids, 'number':number, 'idim':len(asteroids), 'jdim':len(asteroids[0])}
 
 
 # Receives a double array of asteroids and finds the best location
 # for a station and the number of asteroids visible from there.
-def findBestLocation(asteroids, number):
-
-    # Keep the dimensions of the board.
-    idim = len(asteroids)
-    jdim = len(asteroids[0])
+def findBestLocation(grid):
+    asteroids = grid['asteroids']
+    number = grid['number']
+    idim = grid['idim']
+    jdim = grid['jdim']
 
     # Number of asteroids visible from the best spot.
     bestVisibles = -1
@@ -120,6 +146,88 @@ def printBoard(asteroids, idim, jdim):
         print()
     print()
 
-# Call the main function
+# Receives the location of the monitoring station and finds the 
+# asteroid that gets vaporized in 200th place.
+def findVaporized200(grid, x, y):
+    asteroids = grid['asteroids']
+    idim = grid['idim']
+    jdim = grid['jdim']
+
+    # Get the coordinates of the station.
+    istation = y
+    jstation = x
+    xstation = x
+    ystation = idim - istation - 1
+
+    # Parse an angle for each asteroid.
+    angles = []
+    for i in range(idim):
+        for j in range(jdim):
+            # Skip if not an asteroid or the asteroid is the station.
+            if not asteroids[i][j].asteroid:
+                continue
+            if i == istation and j == jstation:
+                continue
+
+            # Create a new angle object and add it to the list.
+            angles.append(AngleInfo(j, idim - i - 1, xstation, ystation))
+
+    # Create a default dictionary, where each value, if it does not exist,
+    # gets an empty list by default.
+    anglesDict = defaultdict(list)
+    for angle in angles:
+        anglesDict[angle.angle].append(angle)
+
+    # Transform the dict to a double list.
+    finalList = [[k, v] for k, v in anglesDict.items()]
+
+    # Order the previous list by value of the angle.
+    finalList.sort()
+
+    # Order the list for each angle by the distance to the station.
+    for element in finalList:
+        element[1].sort(key=orderByDistance)
+
+    # Remove elements from the list following value of angle
+    # and then distance to the station.
+    vaporized = 0
+    length = len(finalList)
+    position = 0
+    while vaporized < 200:
+        if len(finalList[position][1]) != 0:
+            last = finalList[position][1].pop(0)
+            vaporized += 1
+        position = (position + 1) % length
+    
+    # The asteroid vaporized in 200th place is the last one.
+    return {'x':last.x, 'y':idim - last.y - 1}
+
+
+# Given the position of the asteroid and the station, get the
+# angle starting from vertical, clockwise.
+def angle(xstation, ystation, x, y):
+    # Calculate angle according to the standard system.
+    if x - xstation == 0:
+        angle = math.pi / 2 if y - ystation > 0 else - math.pi / 2
+    elif x - xstation >= 0:
+        angle = math.atan((y - ystation) / (x - xstation))
+    else:
+        angle = math.pi + math.atan((y - ystation) / (x - xstation))
+
+    # Now transform this angle to start at zero at vertical and go clockwise.
+    angle = math.pi / 2 - angle
+    
+    # Return the angle with a positive value.
+    return angle if angle >= 0 else angle + 2 * math.pi
+
+
+# Get the distance between the station and the asteroid.
+def distance(xstation, ystation, x, y):
+    return abs(x - xstation) + abs(y - ystation)
+
+def orderByDistance(info):
+    return info.dist
+
+# Call the main function if the module is ran.
 if __name__ == '__main__':
-    main(FILENAME)
+    main(FILENAME, full=True)
--- 10/test_problem10.py
+++ 10/test_problem10.py
@@ -3,15 +3,23 @@ import problem10
 
 class TestAsteroids(unittest.TestCase):
     def testBestLocation(self):
-        bestx = [3, 5, 1, 6, 11]
-        besty = [4, 8, 2, 3, 13]
-        visibles = [8, 33, 35, 41, 210]
+        bestx = [3, 5, 1, 6]
+        besty = [4, 8, 2, 3]
+        visibles = [8, 33, 35, 41]
         for i in range(4):
-            result = problem10.main('tests/test' + str(i + 1) + '.dat')
+            result = problem10.main('tests/test' + str(i + 1) + '.dat', full=False)
             self.assertEqual(result['bestx'], bestx[i])
             self.assertEqual(result['besty'], besty[i])
             self.assertEqual(result['visibles'], visibles[i])
 
-# Call the main function
+    def testBestLocationAndVaporized(self):
+        result = problem10.main('tests/test5.dat', full=True)
+        self.assertEqual(result['bestx'], 11)
+        self.assertEqual(result['besty'], 13)
+        self.assertEqual(result['visibles'], 210)
+        self.assertEqual(result['vaporizedx'], 8)
+        self.assertEqual(result['vaporizedy'], 2)
+
+# Call the main function when the file is executed.
 if __name__ == '__main__':
     unittest.main()
